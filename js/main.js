0b49114b150599df7c72bc6d380458191ab7f18d
Anish Athalye <me@anishathalye.com>
2016-06-11 22:58:49 -0700

    Add more fun

--- js/main.js
+++ js/main.js
@@ -1,13 +1,49 @@
 (function() {
     var LOVE_CHANGE_TIME = 3000;
+    var AUTHORS = [
+        'the HackMIT Team',
+        'clowns',
+        'space monkeys',
+        'dragons',
+        'the Kimberli Zhong squad',
+        'a PRNG',
+        'a thousand monkeys with a thousand keyboards',
+        'doge',
+        'nyan cat',
+        'the hivemind',
+        'hackbot',
+        'Kevin Kwok',
+        'left-pad',
+        'twitch',
+        'a Mersenne twister',
+        'HELP I\'M TRAPPED IN A WEBSITE FACTORY',
+        'the Illuminati',
+        'Beyonc\u00e9',
+        'Jack Florey',
+        'Oliver Smoot',
+        'Ben Bitdiddle',
+        'Alyssa P. Hacker',
+        'John Harvard',
+    ];
+
+    // uniform int from [min, max)
+    var uniform = function(min, max) {
+        return min + Math.floor(Math.random() * (max - min));
+    };
+
+    var choice = function(arr) {
+        return arr[uniform(0, arr.length)];
+    };
 
     var update = function() {
         var love = document.getElementById('love');
-        // dingbats are 0x2700 to 0x27bf
-        var offset = Math.floor(Math.random() * 0xc0);
-        var symbol = String.fromCharCode(0x2700 + offset);
+
+        var symbol = String.fromCharCode(uniform(0x2700, 0x27c0));
+
+        var author = choice(AUTHORS);
+
         love.innerHTML = 'Made with <span class="love-icon">' +
-            symbol + '</span> by the HackMIT Team';
+            symbol + '</span> by ' + author;
     };
 
     setInterval(update, LOVE_CHANGE_TIME);
